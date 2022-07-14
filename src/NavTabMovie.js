cf11876dc2616670c3319658855f671b58b894dc
Anderson <anderson.beltran@hotmail.es>
2022-07-14 16:37:56 -0500

    modificacion

--- src/NavTabMovie.js
+++ src/NavTabMovie.js
@@ -82,19 +82,21 @@ export class NavTabMovie extends LitElement {
     return html`
       <nav>
         <div class="nav nav-tabs" id="nav-tab" role="tablist">
-          <button class="nav-item nav-link ${this.activedTab[0]}" id="nav-review-tab" data-toggle="tab" href="#nav-review" role="tab" aria-controls="nav-review" @click=${this._activeReview}>Review</button>
+          
           <button class="nav-item nav-link ${this.activedTab[1]}" id="nav-actors-tab" data-toggle="tab" href="#nav-actors" role="tab" aria-controls="nav-actors" @click=${this._activeActors}>Actors</button>
           <button class="nav-item nav-link disabled" id="nav-contact-tab" data-toggle="tab" href="#nav-contact" role="tab" aria-controls="nav-contact" @click=${this._activeContact}>Contact</button>
+          <button class="nav-item nav-link ${this.activedTab[0]}" id="nav-review-tab" data-toggle="tab" href="#nav-review" role="tab" aria-controls="nav-review" @click=${this._activeReview}>Review</button>
         </div>
       </nav>
       <div class="tab-content" id="nav-tabContent">
-        <div class="tab-pane fade ${this.activedContent[0]}" id="nav-review" role="tabpanel" aria-labelledby="nav-review-tab">
-            <list-review-card .dataList=${this.dataList[0].data}></list-review-card>
-        </div>
+        
         <div class="tab-pane fade ${this.activedContent[1]}" id="nav-actors" role="tabpanel" aria-labelledby="nav-actors-tab">
             <list-actor-card .dataList=${this.dataList[1].data} ></list-actor-card>
           </div>
         <div class="tab-pane fade disabled" id="nav-contact" role="tabpanel" aria-labelledby="nav-contact-tab">Data de Contact</div>
+        <div class="tab-pane fade ${this.activedContent[0]}" id="nav-review" role="tabpanel" aria-labelledby="nav-review-tab">
+            <list-review-card .dataList=${this.dataList[0].data}></list-review-card>
+        </div>
       </div>
     `;
   }
